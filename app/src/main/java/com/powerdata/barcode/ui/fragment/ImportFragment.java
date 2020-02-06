a823734e95342a62d681f2742e72d03088e32b07
Cao Chun <kalavinka223@163.com>
2020-02-06 14:57:14 +0800

    update import fragment

--- app/src/main/java/com/powerdata/barcode/ui/fragment/ImportFragment.java
+++ app/src/main/java/com/powerdata/barcode/ui/fragment/ImportFragment.java
@@ -1,23 +1,34 @@
 package com.powerdata.barcode.ui.fragment;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.powerdata.barcode.R;
 import com.powerdata.barcode.databinding.FragmentImportBinding;
 import com.powerdata.barcode.viewModel.ImportViewModel;
 
+import java.io.IOException;
+
 public class ImportFragment extends Fragment {
+
+    private static final int PICK_FILE = 1;
 
     private ImportViewModel viewModel;
 
@@ -34,7 +45,7 @@ public class ImportFragment extends Fragment {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                viewModel.getShipNoItemPosition().setValue(position);
+                viewModel.shipNoItemPosition.setValue(position);
             }
 
             @Override
@@ -42,6 +53,17 @@ public class ImportFragment extends Fragment {
 
             }
         });
+
+        viewModel.openDocumentAction
+                .observe(getViewLifecycleOwner(), new Observer<Void>() {
+                    @Override
+                    public void onChanged(Void aVoid) {
+                        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                        intent.addCategory(Intent.CATEGORY_OPENABLE);
+                        intent.setType("*/*");
+                        startActivityForResult(intent, PICK_FILE);
+                    }
+                });
 
         return root;
     }
@@ -51,4 +73,29 @@ public class ImportFragment extends Fragment {
         inflater.inflate(R.menu.save_menu, menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.save_item) {
+            viewModel.onSaveButtonClick();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == PICK_FILE && resultCode == Activity.RESULT_OK && data != null) {
+            try {
+                Uri uri = data.getData();
+                assert uri != null;
+                ParcelFileDescriptor pfd = requireContext().getContentResolver().
+                        openFileDescriptor(uri, "r");
+                assert pfd != null;
+                viewModel.file.setValue(pfd.getFileDescriptor());
+                pfd.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- app/src/main/java/com/powerdata/barcode/viewModel/ImportViewModel.java
+++ app/src/main/java/com/powerdata/barcode/viewModel/ImportViewModel.java
@@ -7,14 +7,42 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 
+import com.powerdata.barcode.MyApplication;
 import com.powerdata.barcode.common.Constant;
+import com.powerdata.barcode.common.SingleLiveEvent;
+import com.powerdata.barcode.common.Util;
+import com.powerdata.barcode.model.BarcodeDetail;
+import com.powerdata.barcode.repository.BarcodeDetailDao;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.FileDescriptor;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.schedulers.Schedulers;
 
 public class ImportViewModel extends ViewModel {
 
-    private MutableLiveData<Integer> shipNoItemPosition = new MutableLiveData<>();
-    private MutableLiveData<String> barcode = new MutableLiveData<>();
+    public MutableLiveData<Integer> shipNoItemPosition = new MutableLiveData<>();
+    public MutableLiveData<String> barcode = new MutableLiveData<>();
+    public SingleLiveEvent<Void> openDocumentAction = new SingleLiveEvent<>();
+    public MutableLiveData<FileDescriptor> file = new MutableLiveData<>();
+    public LiveData<String> totalCount;
+    public LiveData<String> scannedCount;
+    public LiveData<String> notScannedCount;
 
     private LiveData<String> shipNo;
+
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+    private BarcodeDetailDao detailDao = MyApplication.db.barcodeDetailDao();
 
     public ImportViewModel() {
         shipNo = Transformations.map(shipNoItemPosition, new Function<Integer, String>() {
@@ -23,13 +51,83 @@ public class ImportViewModel extends ViewModel {
                 return Constant.SHIP_NO_ARRAY[input];
             }
         });
+
+        file.observeForever(new Observer<FileDescriptor>() {
+            @Override
+            public void onChanged(FileDescriptor file) {
+                importCSV(file);
+            }
+        });
+
+        Function<Integer, String> int2StrFunc = new Function<Integer, String>() {
+            @Override
+            public String apply(Integer input) {
+                return String.valueOf(input);
+            }
+        };
+
+        totalCount = Transformations.map(Transformations.switchMap(shipNo, new Function<String, LiveData<Integer>>() {
+            @Override
+            public LiveData<Integer> apply(String s) {
+                return Util.transform(detailDao.countByShiNo(s), compositeDisposable);
+            }
+        }), int2StrFunc);
+
+        scannedCount = Transformations.map(Transformations.switchMap(shipNo, new Function<String, LiveData<Integer>>() {
+            @Override
+            public LiveData<Integer> apply(String s) {
+                return Util.transform(detailDao.countByShiNoAndStatus(s, 1), compositeDisposable);
+            }
+        }), int2StrFunc);
+
+        notScannedCount = Transformations.map(Transformations.switchMap(shipNo, new Function<String, LiveData<Integer>>() {
+            @Override
+            public LiveData<Integer> apply(String s) {
+                return Util.transform(detailDao.countByShiNoAndStatus(s, 0), compositeDisposable);
+            }
+        }), int2StrFunc);
     }
 
-    public MutableLiveData<Integer> getShipNoItemPosition() {
-        return shipNoItemPosition;
+    public void onSaveButtonClick() {
+
     }
 
-    public MutableLiveData<String> getBarcode() {
-        return barcode;
+    public void onImportButtonClick() {
+        openDocumentAction.call();
     }
+
+    private void importCSV(FileDescriptor file) {
+        try {
+            Reader in = new FileReader(file);
+            Iterable<CSVRecord> records = CSVFormat.RFC4180.withHeader().parse(in);
+            List<BarcodeDetail> list = new ArrayList<>();
+
+            for (CSVRecord record : records) {
+                BarcodeDetail detail = new BarcodeDetail();
+                detail.barcode = record.get(1);
+                detail.thickness = Float.valueOf(record.get(2));
+                detail.weight = Float.valueOf(record.get(3));
+                detail.pileNo = record.get(4);
+                detail.mark = record.get(5);
+                detail.createdAt = Util.formatDate(System.currentTimeMillis());
+                detail.shipNo = shipNo.getValue();
+                detail.status = 0;
+                list.add(detail);
+            }
+
+            Disposable disposable = detailDao.inserts(list)
+                    .subscribeOn(Schedulers.io())
+                    .subscribe(new Action() {
+                        @Override
+                        public void run() {
+                        }
+                    });
+
+            compositeDisposable.add(disposable);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
