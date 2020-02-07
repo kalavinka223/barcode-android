7b8f7b97a9ab828ff2e5813036be4e29209bf766
Cao Chun <kalavinka223@163.com>
2020-02-07 17:41:37 +0800

    update barcode collection

--- app/src/main/java/com/powerdata/barcode/ui/fragment/BarcodeCollectionFragment.java
+++ app/src/main/java/com/powerdata/barcode/ui/fragment/BarcodeCollectionFragment.java
@@ -1,6 +1,10 @@
 package com.powerdata.barcode.ui.fragment;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,10 +16,11 @@ import android.widget.Button;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -25,11 +30,12 @@ import com.powerdata.barcode.model.BarcodeInfo;
 import com.powerdata.barcode.ui.adapter.BarcodeAdapter;
 import com.powerdata.barcode.viewModel.BarcodeCollectionViewModel;
 
+import java.io.IOException;
 import java.util.List;
 
 public class BarcodeCollectionFragment extends Fragment {
 
-    static final String ARG_SHIP_NO = "arg_ship_no";
+    private static final int CREATE_FILE = 1;
 
     private BarcodeCollectionViewModel viewModel;
 
@@ -59,6 +65,26 @@ public class BarcodeCollectionFragment extends Fragment {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
 
+        Button deleteButton = root.findViewById(R.id.delete_button);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDeleteAlertDialog();
+            }
+        });
+
+        Button exportButton = root.findViewById(R.id.export_button);
+        exportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("text/csv");
+                intent.putExtra(Intent.EXTRA_TITLE, "直接采集.csv");
+                startActivityForResult(intent, CREATE_FILE);
+            }
+        });
+
         viewModel.getBarcodeInfos()
                 .observe(this, new Observer<List<BarcodeInfo>>() {
                     @Override
@@ -82,6 +108,41 @@ public class BarcodeCollectionFragment extends Fragment {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == CREATE_FILE && resultCode == Activity.RESULT_OK && data != null) {
+            try {
+                Uri uri = data.getData();
+                assert uri != null;
+                ParcelFileDescriptor pfd = requireContext().getContentResolver()
+                        .openFileDescriptor(uri, "w");
+                assert pfd != null;
+                viewModel.export(pfd.getFileDescriptor());
+                pfd.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void showDeleteAlertDialog() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.setTitle(getString(R.string.text_delete));
+        dialog.setMessage(getString(R.string.message_delete_alert));
+        dialog.show(requireFragmentManager(), "delete_alert_dialog");
+        dialog.setListener(new AlertDialogFragment.AlertDialogListener() {
+            @Override
+            public void onDialogPositiveClick(DialogFragment dialog) {
+                viewModel.delete();
+            }
+
+            @Override
+            public void onDialogNegativeClick(DialogFragment dialog) {
+
+            }
+        });
     }
 
 }
--- app/src/main/java/com/powerdata/barcode/viewModel/BarcodeCollectionViewModel.java
+++ app/src/main/java/com/powerdata/barcode/viewModel/BarcodeCollectionViewModel.java
@@ -1,8 +1,9 @@
 package com.powerdata.barcode.viewModel;
 
+import androidx.arch.core.util.Function;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.Observer;
+import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 
 import com.powerdata.barcode.MyApplication;
@@ -11,10 +12,18 @@ import com.powerdata.barcode.common.Util;
 import com.powerdata.barcode.model.BarcodeInfo;
 import com.powerdata.barcode.repository.BarcodeInfoDao;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.List;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Action;
@@ -23,25 +32,27 @@ import io.reactivex.schedulers.Schedulers;
 public class BarcodeCollectionViewModel extends ViewModel {
 
     public MutableLiveData<String> barcode = new MutableLiveData<>();
-    private MutableLiveData<List<BarcodeInfo>> barcodeInfos = new MutableLiveData<>();
-    private String shipNo = Constant.SHIP_NO_ARRAY[0];
+    private LiveData<List<BarcodeInfo>> barcodeInfos;
+    private MutableLiveData<String> shipNo = new MutableLiveData<>();
 
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
     private BarcodeInfoDao infoDao = MyApplication.db.barcodeInfoDao();
 
+    public BarcodeCollectionViewModel() {
+        barcodeInfos = Transformations.switchMap(shipNo, new Function<String, LiveData<List<BarcodeInfo>>>() {
+            @Override
+            public LiveData<List<BarcodeInfo>> apply(String s) {
+                return infoDao.listByShipNo(s);
+            }
+        });
+    }
+
     public void setShipNoItemPosition(int position) {
-        shipNo = Constant.SHIP_NO_ARRAY[position];
-        infoDao.listByShipNo(shipNo)
-                .observeForever(new Observer<List<BarcodeInfo>>() {
-                    @Override
-                    public void onChanged(List<BarcodeInfo> list) {
-                        barcodeInfos.setValue(list);
-                    }
-                });
+        shipNo.setValue(Constant.SHIP_NO_ARRAY[position]);
     }
 
     public String getShipNo() {
-        return shipNo;
+        return shipNo.getValue();
     }
 
     public LiveData<List<BarcodeInfo>> getBarcodeInfos() {
@@ -53,7 +64,7 @@ public class BarcodeCollectionViewModel extends ViewModel {
         barcode.setValue("");
         if (StringUtils.isNotEmpty(code)) {
             BarcodeInfo info = new BarcodeInfo();
-            info.shipNo = shipNo;
+            info.shipNo = shipNo.getValue();
             info.barcode = code;
             info.createdAt = Util.formatDate(System.currentTimeMillis());
             Disposable disposable = infoDao.insert(info)
@@ -65,6 +76,39 @@ public class BarcodeCollectionViewModel extends ViewModel {
                         }
                     });
             compositeDisposable.add(disposable);
+        }
+    }
+
+    public void delete() {
+        Disposable disposable = infoDao.deleteByShipNo(shipNo.getValue())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action() {
+                    @Override
+                    public void run() {
+
+                    }
+                });
+        compositeDisposable.add(disposable);
+    }
+
+    public void export(FileDescriptor file) {
+        try {
+            List<BarcodeInfo> list = barcodeInfos.getValue();
+            if (list != null) {
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                OutputStreamWriter writer = new OutputStreamWriter(fileOutputStream, "GBK");
+                Appendable printWriter = new PrintWriter(writer);
+                CSVPrinter csvPrinter = CSVFormat.EXCEL.withHeader("钢卷号", "扫描时间", "船号").print(printWriter);
+                for (int i = 0; i < list.size(); i++) {
+                    BarcodeInfo barcodeInfo = list.get(i);
+                    csvPrinter.printRecord(barcodeInfo.barcode, barcodeInfo.createdAt, barcodeInfo.shipNo);
+                }
+                csvPrinter.flush();
+                csvPrinter.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
