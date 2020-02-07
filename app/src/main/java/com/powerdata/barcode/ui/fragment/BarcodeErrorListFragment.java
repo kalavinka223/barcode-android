0241d454131ad887663700725283b339809b1727
Cao Chun <kalavinka223@163.com>
2020-02-07 19:59:12 +0800

    update barcode error list

--- app/src/main/java/com/powerdata/barcode/ui/fragment/BarcodeErrorListFragment.java
+++ app/src/main/java/com/powerdata/barcode/ui/fragment/BarcodeErrorListFragment.java
@@ -1,11 +1,18 @@
 package com.powerdata.barcode.ui.fragment;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -18,6 +25,7 @@ import com.powerdata.barcode.model.BarcodeError;
 import com.powerdata.barcode.ui.adapter.BarcodeErrorAdapter;
 import com.powerdata.barcode.viewModel.BarcodeErrorListViewModel;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
@@ -56,6 +64,26 @@ public class BarcodeErrorListFragment extends Fragment {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
 
+        Button deleteButton = root.findViewById(R.id.delete_button);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDeleteAlertDialog();
+            }
+        });
+
+        Button exportButton = root.findViewById(R.id.export_button);
+        exportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("text/csv");
+                intent.putExtra(Intent.EXTRA_TITLE, "船" + shipNo + "失败明细.csv");
+                startActivityForResult(intent, CREATE_FILE);
+            }
+        });
+
         viewModel.getErrors()
                 .observe(this, new Observer<List<BarcodeError>>() {
                     @Override
@@ -67,6 +95,41 @@ public class BarcodeErrorListFragment extends Fragment {
         viewModel.load(shipNo);
 
         return root;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == CREATE_FILE && resultCode == Activity.RESULT_OK && data != null) {
+            try {
+                Uri uri = data.getData();
+                assert uri != null;
+                ParcelFileDescriptor pfd = requireContext().getContentResolver()
+                        .openFileDescriptor(uri, "w");
+                assert pfd != null;
+                viewModel.export(pfd.getFileDescriptor());
+                pfd.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void showDeleteAlertDialog() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.setTitle(getString(R.string.text_delete));
+        dialog.setMessage(getString(R.string.message_delete_alert));
+        dialog.show(requireFragmentManager(), "delete_alert_dialog");
+        dialog.setListener(new AlertDialogFragment.AlertDialogListener() {
+            @Override
+            public void onDialogPositiveClick(DialogFragment dialog) {
+                viewModel.delete();
+            }
+
+            @Override
+            public void onDialogNegativeClick(DialogFragment dialog) {
+
+            }
+        });
     }
 
 }
--- app/src/main/java/com/powerdata/barcode/viewModel/BarcodeErrorListViewModel.java
+++ app/src/main/java/com/powerdata/barcode/viewModel/BarcodeErrorListViewModel.java
@@ -9,9 +9,24 @@ import com.powerdata.barcode.MyApplication;
 import com.powerdata.barcode.model.BarcodeError;
 import com.powerdata.barcode.repository.BarcodeErrorDao;
 
-import java.util.List;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.schedulers.Schedulers;
 
 public class BarcodeErrorListViewModel extends ViewModel {
 
@@ -33,6 +48,57 @@ public class BarcodeErrorListViewModel extends ViewModel {
                         errors.setValue(list);
                     }
                 });
+    }
+
+    public void delete() {
+        Disposable disposable = errorDao.deleteByShipNo(shipNo)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action() {
+                    @Override
+                    public void run() {
+
+                    }
+                });
+        compositeDisposable.add(disposable);
+    }
+
+    public void export(final FileDescriptor file) {
+        if (errors.getValue() == null)
+            return;
+
+        FutureTask<Boolean> task = new FutureTask<>(new Callable<Boolean>() {
+            @Override
+            public Boolean call() {
+                try {
+                    List<BarcodeError> list = errors.getValue();
+                    FileOutputStream fileOutputStream = new FileOutputStream(file);
+                    OutputStreamWriter writer = new OutputStreamWriter(fileOutputStream, "GBK");
+                    Appendable printWriter = new PrintWriter(writer);
+                    CSVPrinter csvPrinter = CSVFormat.EXCEL.withHeader("钢卷号", "扫描时间", "船号").print(printWriter);
+                    for (int i = 0; i < list.size(); i++) {
+                        BarcodeError error = list.get(i);
+                        csvPrinter.printRecord(error.barcode, error.createdAt, error.shipNo);
+                    }
+                    csvPrinter.flush();
+                    csvPrinter.close();
+                    return true;
+                } catch (IOException e) {
+                    return false;
+                }
+            }
+        });
+
+        Thread thread = new Thread(task);
+        thread.start();
+
+        try {
+            if (task.get() ) {
+                //listener.onExportSuccess();
+            }
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
