00d8af6765e21a8f109486581c895e561f479f6b
Cao Chun <kalavinka223@163.com>
2020-02-07 15:44:09 +0800

    update barcode list delete

--- app/src/main/java/com/powerdata/barcode/viewModel/BarcodeListViewModel.java
+++ app/src/main/java/com/powerdata/barcode/viewModel/BarcodeListViewModel.java
@@ -9,9 +9,14 @@ import com.powerdata.barcode.MyApplication;
 import com.powerdata.barcode.model.BarcodeInfo;
 import com.powerdata.barcode.repository.BarcodeInfoDao;
 
+import java.io.FileDescriptor;
 import java.util.List;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.schedulers.Schedulers;
 
 public class BarcodeListViewModel extends ViewModel {
 
@@ -34,6 +39,23 @@ public class BarcodeListViewModel extends ViewModel {
                         barcodeInfos.setValue(list);
                     }
                 });
+    }
+
+    public void delete() {
+        Disposable disposable = infoDao.deleteByShipNo(shipNo)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action() {
+                    @Override
+                    public void run() {
+
+                    }
+                });
+        compositeDisposable.add(disposable);
+    }
+
+    public void export(FileDescriptor file) {
+
     }
 
 }
